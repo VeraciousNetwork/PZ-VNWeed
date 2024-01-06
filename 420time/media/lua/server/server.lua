aa290ed2a65435e95376cf3da4d8d6f42bd5a317
nofacetv <74198524+nofacetv@users.noreply.github.com>
2024-01-06 15:11:59 -0500

    More Ground works

--- 420time/media/lua/server/server.lua
+++ 420time/media/lua/server/server.lua
@@ -8,6 +8,19 @@ module NoFaces420time {
     [Recipe.GetItemTypes.Medical]=2
         function Recipe.GetItemTypes.Medical(scriptItems)
         scriptItems:addAll(getScriptManager():getItemsTag("Marijuana"))
+    end
+
+    OnTest:Recipe.OnTest.IsNotWorn,
+    function Recipe.OnTest.IsNotWorn(item)
+        if instanceof(item, "Marijauna") then
+        return not item:canUse()
         end
-            fu
+        return true
+    end
+
+    OnCreate:Recipe.OnCreate.Dismantle,
+
+    function Recipe.onCreate.Dismantle(items, result, player)
+        player:getInventory():AddItem("Base.BrokenGlass");
+    end
 }
