5adb09761da18556ba47308ba5b0b460a1121fbe
nofacetv <74198524+nofacetv@users.noreply.github.com>
2024-01-06 14:58:32 -0500

    Add files via upload

--- 420time/media/lua/server/server.lua
+++ 420time/media/lua/server/server.lua
@@ -1,1 +1,13 @@
-
+--imports Base
+module NoFaces420time {
+}
+    imports {
+        Base
+    }
+-- Fetches Item Tags
+    [Recipe.GetItemTypes.Medical]=2
+        function Recipe.GetItemTypes.Medical(scriptItems)
+        scriptItems:addAll(getScriptManager():getItemsTag("Marijuana"))
+        end
+            fu
+}
